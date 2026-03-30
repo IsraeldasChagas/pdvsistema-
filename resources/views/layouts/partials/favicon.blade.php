a61f9404039bdf6360d65f6beb458b1c116cdb97
Israel Chagas <seuemail@gmail.com>
2026-03-30 15:48:56 -0400

    AtualizaÃ§Ã£o 2026-03-30 15:48

--- resources/views/layouts/partials/favicon.blade.php
+++ resources/views/layouts/partials/favicon.blade.php
@@ -1,27 +1,22 @@
 @php
-    $icon = null;
-    $base = public_path();
-    foreach (['icon.ico', 'icon.png', 'icon.svg', 'icon.jpg', 'icon.jpeg', 'icon.webp'] as $f) {
-        if (is_file($base . DIRECTORY_SEPARATOR . $f)) {
-            $icon = $f;
-            break;
-        }
-    }
+    $dir = public_path('favicon');
+    $stamp = is_file($dir . DIRECTORY_SEPARATOR . 'favicon.ico')
+        ? (string) filemtime($dir . DIRECTORY_SEPARATOR . 'favicon.ico')
+        : (string) time();
 @endphp
-@if ($icon !== null)
-    @php
-        $ext = strtolower(pathinfo($icon, PATHINFO_EXTENSION));
-        $type = match ($ext) {
-            'ico' => 'image/x-icon',
-            'png' => 'image/png',
-            'svg' => 'image/svg+xml',
-            'jpg', 'jpeg' => 'image/jpeg',
-            'webp' => 'image/webp',
-            default => 'image/png',
-        };
-        $m = (string) filemtime($base . DIRECTORY_SEPARATOR . $icon);
-    @endphp
-    <link rel="icon" href="{{ asset($icon) }}?v={{ $m }}" type="{{ $type }}" sizes="any">
-    <link rel="shortcut icon" href="{{ asset($icon) }}?v={{ $m }}" type="{{ $type }}">
-    <link rel="apple-touch-icon" href="{{ asset($icon) }}?v={{ $m }}">
+@if (is_file($dir . DIRECTORY_SEPARATOR . 'favicon.ico'))
+    <link rel="icon" href="{{ asset('favicon/favicon.ico') }}?v={{ $stamp }}" sizes="any">
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}?v={{ $stamp }}" type="image/x-icon">
+@endif
+@if (is_file($dir . DIRECTORY_SEPARATOR . 'favicon-32x32.png'))
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}?v={{ $stamp }}">
+@endif
+@if (is_file($dir . DIRECTORY_SEPARATOR . 'favicon-16x16.png'))
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}?v={{ $stamp }}">
+@endif
+@if (is_file($dir . DIRECTORY_SEPARATOR . 'apple-touch-icon.png'))
+    <link rel="apple-touch-icon" href="{{ asset('favicon/apple-touch-icon.png') }}?v={{ $stamp }}">
+@endif
+@if (is_file($dir . DIRECTORY_SEPARATOR . 'site.webmanifest'))
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}?v={{ $stamp }}">
 @endif
