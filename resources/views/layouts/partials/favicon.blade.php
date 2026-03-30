25f9bc7faca4cc48f9b71249dc81047a2d59d2e7
Israel Chagas <seuemail@gmail.com>
2026-03-30 15:21:21 -0400

    AtualizaÃ§Ã£o 2026-03-30 15:21

--- resources/views/layouts/partials/favicon.blade.php
+++ resources/views/layouts/partials/favicon.blade.php
@@ -1,16 +1,19 @@
 @php
     $dir = public_path('imagem');
-    $faviconRel = null;
+    $override = null;
     foreach (['pdv.ico', 'pdv.png', 'pdv.jpg', 'pdv.jpeg', 'pdv.webp'] as $f) {
         if (is_file($dir . DIRECTORY_SEPARATOR . $f)) {
-            $faviconRel = 'imagem/' . $f;
+            $override = 'imagem/' . $f;
             break;
         }
     }
+
+    $svgPath = public_path('favicon.svg');
+    $svgV = is_file($svgPath) ? (string) filemtime($svgPath) : '1';
 @endphp
-@if ($faviconRel !== null)
+@if ($override !== null)
     @php
-        $ext = strtolower(pathinfo($faviconRel, PATHINFO_EXTENSION));
+        $ext = strtolower(pathinfo($override, PATHINFO_EXTENSION));
         $type = match ($ext) {
             'ico' => 'image/x-icon',
             'png' => 'image/png',
@@ -19,6 +22,11 @@
             default => 'image/png',
         };
     @endphp
-    <link rel="icon" href="{{ asset($faviconRel) }}" type="{{ $type }}" sizes="any">
-    <link rel="apple-touch-icon" href="{{ asset($faviconRel) }}">
+    @php $ovM = (string) filemtime(public_path($override)); @endphp
+    <link rel="icon" href="{{ asset($override) }}?v={{ $ovM }}" type="{{ $type }}" sizes="any">
+    <link rel="apple-touch-icon" href="{{ asset($override) }}?v={{ $ovM }}">
+@else
+    <link rel="icon" href="{{ asset('favicon.svg') }}?v={{ $svgV }}" type="image/svg+xml" sizes="any">
+    <link rel="shortcut icon" href="{{ asset('favicon.svg') }}?v={{ $svgV }}" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.svg') }}?v={{ $svgV }}">
 @endif
